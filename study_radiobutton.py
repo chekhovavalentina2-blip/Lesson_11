f3fb17324aa4295346e902102f4d29e10ef98de6
chekhovavalentina2-blip <chekhovavalentina2@gmail.com>
2025-08-16 15:21:27 +0300

    +

--- study_radiobutton.py
+++ study_radiobutton.py
@@ -4,7 +4,7 @@ from tkinter import messagebox
 
 def study_radio():
 
-    window = tk.Tk()
+    window = tk.Toplevel()
     window.title("Радиокнопки")
     window.geometry("300x200")
 
@@ -12,14 +12,13 @@ def study_radio():
 
     tk.Label(window, text="Выберите язык").pack(pady=5)
     tk.Radiobutton(window, text="Python", variable=var, value="python").pack()
-
     tk.Radiobutton(window, text="Java", variable=var, value="java").pack()
-    tk.Radiobutton(window, text="C++", variable=var, value="c++").pack()
+    tk.Radiobutton(window, text="C++", variable=var, value="cpp").pack()
 
 
-    def show_values():
+    def show_coice():
         messagebox.showinfo("Результат", f"Выбран: {var.get()}")
 
 
-    tk.Button(window, text="Показать", command=show_values).pack(pady=10)
+    tk.Button(window, text="Показать", command=show_coice).pack(pady=10)
 
