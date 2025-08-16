d9e2b950dee70875bc6f86318ba8b854e330201a
chekhovavalentina2-blip <chekhovavalentina2@gmail.com>
2025-08-16 14:34:49 +0300

    добавила во второе окно пароль - показать пароль

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ if __name__ == '__main__':
     root = tk.Tk()
     root.title("Изучение элементов GUI")
     root.geometry("400x300")
-    tk.Button(root, text="Изучение полей ввода", command=study_entry).pack(pady=20)
+    tk.Button(root, 
+              text="Изучение полей ввода", 
+              command=study_entry).pack(pady=20)
 
     root.mainloop()
