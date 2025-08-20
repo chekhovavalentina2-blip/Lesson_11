32c7241cecb4af17558d50bc64ae398534685a36
chekhovavalentina2-blip <chekhovavalentina2@gmail.com>
2025-08-20 08:45:30 +0300

    debugging

---
+++ debugging.py
@@ -0,0 +1,20 @@
+import logging
+import colorlog 
+
+# Настройка цветного логирования
+handler = colorlog.StreamHandler()
+handler.setFormatter(colorlog.ColoredFormatter(
+    '%(log_color)s%(levelname)s:%(name)s:%(message)s'
+))
+
+logger = colorlog.getLogger('example')
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+logger.propagate = False     # Отключаем передачу в родительский логгер
+
+# Теперь логи будут цветными
+logger.debug("Подробная информация для отладки")      # СЕРЫЙ
+logger.info("Общая информация о рботе")               # БЕЛЫЙ
+logger.warning("Предупреждение о проблеме")           # ЖЕЛТЫЙ
+logger.error("Ошибка в работе программы")             # КРАСНЫЙ
+logger.critical("Критическая ошибка")
